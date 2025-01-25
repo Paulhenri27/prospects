3ba02a15fa6573a63cf96b227c3db0e5db09a16c
Paul Henri Nguepi Djoumessi <pauldjoums@gmail.com>
2025-01-25 15:16:22 +0300

    package modified

--- server.js
+++ server.js
@@ -61,5 +61,8 @@ app.get("/users", async (req, res) => {
   }
 });
 
-module.exports = app;
-
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
